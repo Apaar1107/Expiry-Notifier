dd3ea513dbbf3b04f0cdd40a8a50c428ce3696a6
Apaar SIngh <129654597+Apaar1107@users.noreply.github.com>
2025-02-11 15:42:40 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cookie());
 // CORS Configuration
 app.use(
     cors({
-      origin:"http://localhost:5173",
+      origin:"https://expiry-notifier-ni1c.onrender.com",
       credentials: true,
     })
 );
