3216e2d257584f3761826165ce3e084a703f16b1
Apaar SIngh <129654597+Apaar1107@users.noreply.github.com>
2025-02-11 16:36:23 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const STORE_API_END_POINT="https://expiry-notifier-ni1c.onrender.com/api/v1/store";
-export const RECORD_API_END_POINT="https://expiry-notifier-ni1c.onrender.com/api/v1/record";
+export const STORE_API_END_POINT="https://expiry-notifier-b6ci.onrender.com/api/v1/store";
+export const RECORD_API_END_POINT="https://expiry-notifier-b6ci.onrender.com/api/v1/record";
